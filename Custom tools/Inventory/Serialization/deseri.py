48f603bd7e7478d51084da87d3494b96a53db5ae
yamerooo123 <suprawitch123@email.com>
2024-11-17 17:50:32 +0700

    a little more

--- Custom tools/Inventory/Serialization/deseri.py
+++ Custom tools/Inventory/Serialization/deseri.py
@@ -1,3 +1,4 @@
+# เวลาใช้งานวาง payload ไว้ใน obj แล้วรัน python3 deseri.py
 import pickle5 as pickle
 
 desir_me = b'\x80\x04\x95\x17\x00\x00\x00\x00\x00\x00\x00\x8c\x13serialize me please\x94.'
--- Custom tools/Inventory/Serialization/seri.py
+++ Custom tools/Inventory/Serialization/seri.py
@@ -1,3 +1,4 @@
+# เวลาใช้งานวาง payload ไว้ใน obj แล้วรัน python3 seri.py
 import pickle5 as pickle
 
 obj = "serialize me please"
